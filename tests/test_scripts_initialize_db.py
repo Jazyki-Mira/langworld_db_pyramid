8055266294afa0e5986f1497733bc829e6ecf3dc
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2022-06-16 14:58:12 +0300

    populate DB with countries and doculects in 2 separate loops

--- tests/test_scripts_initialize_db.py
+++ tests/test_scripts_initialize_db.py
@@ -1,3 +1,5 @@
+import logging
+
 from sqlalchemy import select
 
 import langworld_db_pyramid.models as models
@@ -7,11 +9,19 @@ def test_initialize_db(dbsession):
     from langworld_db_pyramid.scripts.initialize_db import setup_models
 
     setup_models(dbsession)
-    result = dbsession.scalars(select(models.Doculect)).all()
 
-    assert len(list(result)) == 429
+    all_doculects = dbsession.scalars(select(models.Doculect)).all()
+    assert len(list(all_doculects)) == 429
 
-    for item in result:
+    all_countries = dbsession.scalars(select(models.Country)).all()
+    assert len(all_countries) == 283
+
+    for item in all_doculects:
         assert item.string_id
         assert isinstance(item.main_country, models.Country)
         assert item.main_country.id
+        # print(item.main_country.id, item.main_country.man_id)
+
+    afg = dbsession.scalars(select(models.Country).where(models.Country.name_en == 'Afghanistan')).one()
+    assert len(afg.doculects) == 21
+
