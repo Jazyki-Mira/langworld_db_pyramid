dc7a0d90ef9ec5900644bc65b635eb0d80176239
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2022-07-02 16:38:12 +0300

    add separate static routes, include React imports into main Jinja template

--- langworld_db_pyramid/routes.py
+++ langworld_db_pyramid/routes.py
@@ -1,5 +1,8 @@
 def includeme(config):
-    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_static_view('css', 'static/css', cache_max_age=3600)
+    config.add_static_view('img', 'static/images', cache_max_age=3600)
+    config.add_static_view('scripts', 'static/js', cache_max_age=3600)
+    config.add_static_view('pdf', 'static/pdf_volumes', cache_max_age=3600)
     config.add_route('all_doculects', '/')
     config.add_route('doculect_profile', '/doculect/{doculect_man_id}')
     config.add_route('doculect_man_ids_containing_substring', '/{locale}/json_api/doculect_by_name/{query}')
