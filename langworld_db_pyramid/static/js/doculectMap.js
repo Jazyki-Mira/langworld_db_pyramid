39746e9e56b33e622e32e84409065a809121a0c0
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2022-07-06 16:51:17 +0300

    refactor: allow custom coordinates for center of map

--- langworld_db_pyramid/static/js/doculectMap.js
+++ langworld_db_pyramid/static/js/doculectMap.js
@@ -1,27 +1,29 @@
 import accessToken from "./leafletAccessToken.js"
 
-let mymap = L.map('doculect-profile-map').setView([55.0, 95.0], 2.5);
-const titleLayerUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + accessToken;
+const renderMarkers = (centerLatitude = 55.0, centerLongitude = 95.0) => {
+    let doculectMap = L.map('doculect-profile-map').setView([centerLatitude, centerLongitude], 2.5);
+    const titleLayerUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + accessToken;
 
-L.tileLayer(titleLayerUrl,
-    {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken: accessToken
-    }
-).addTo(mymap);
+    L.tileLayer(titleLayerUrl,
+        {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: accessToken
+        }
+    ).addTo(doculectMap);
 
-const renderMarkers = () => fetch("../json_api/doculects_for_map/")
-        .then(res => res.json())
-        .then(doculects => addMarkers(doculects))
-        .catch(console.error);
+    fetch("../json_api/doculects_for_map/")
+    .then(res => res.json())
+    .then(doculects => addMarkers(doculects, doculectMap))
+    .catch(console.error);
+}
 
-const addMarkers = doculects => {
+const addMarkers = (doculects, doculectMap) => {
     for (let doculect of doculects) {
-        let marker = L.marker([doculect["latitude"], doculect["longitude"]], {icon: L.divIcon({className: "div-icon square-with-outline green"})}).addTo(mymap);
+        let marker = L.marker([doculect["latitude"], doculect["longitude"]], {icon: L.divIcon({className: "div-icon square-with-outline green"})}).addTo(doculectMap);
         let url = `../doculect/${doculect["id"]}`;
         marker.bindPopup("<a href=" + url + ">" + doculect["name"] + "</a>");
         marker.on("mouseover", function (e) { this.openPopup(); });
