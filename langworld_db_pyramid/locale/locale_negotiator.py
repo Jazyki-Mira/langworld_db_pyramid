80480e1ad57f3ad5a0a62240693e7f48c07bc68b
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2022-07-07 16:57:23 +0300

    fix locale_negotiator.py: matchdict can be None

--- langworld_db_pyramid/locale/locale_negotiator.py
+++ langworld_db_pyramid/locale/locale_negotiator.py
@@ -1,10 +1,16 @@
 from pyramid.settings import aslist
 
 
+DEFAULT_LOCALE = 'ru'
+
+
 def locale_negotiator_from_url(request):
+    if not request.matchdict:
+        return DEFAULT_LOCALE
+
     locale = request.matchdict.get('locale', 'ru')
 
     if locale not in aslist(request.registry.settings['available_languages']):
-        locale = 'ru'
+        return DEFAULT_LOCALE
 
     return locale
