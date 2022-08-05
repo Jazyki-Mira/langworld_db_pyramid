3efd655dc53b6cc38791a63690ffde2187ded61f
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2022-08-06 00:15:37 +0300

    refactor(doculectListForm.js): rm call to `renderListOfMatchingDoculects`
    * the call to renderListOfMatchingDoculects is not needed
    because `retrieve_data` will call it anyway
    after the data is fetched.
    
    * rename `retrieve_data`
    
    all other changes are cosmetic, made by Prettier

--- langworld_db_pyramid/static/js/doculectListForm.js
+++ langworld_db_pyramid/static/js/doculectListForm.js
@@ -1,55 +1,67 @@
-import DoculectListItem from "./doculectListItem.js"
-import doculectListFormLabels from "./i18n/doculectListFormLabels.js"
-import getLocale from "./getLocale.js"
+import DoculectListItem from "./doculectListItem.js";
+import doculectListFormLabels from "./i18n/doculectListFormLabels.js";
+import getLocale from "./getLocale.js";
 
 const elem = React.createElement;
 
-const retrieve_data = url => fetch(url)
-        .then(res => res.json())
-        .then(renderListOfMatchingDoculects)
-        .catch(console.error);
+const fetchAndRenderResults = (url) =>
+  fetch(url)
+    .then((res) => res.json())
+    .then(renderListOfMatchingDoculects)
+    .catch(console.error);
 
-const renderListOfMatchingDoculects = retrieved_data => {
+const renderListOfMatchingDoculects = (retrieved_data) => {
+  if (typeof retrieved_data[Symbol.iterator] != "function") return; // promise is pending, no data to iterate over
 
-    if (typeof retrieved_data[Symbol.iterator] != 'function') return;  // promise is pending, no data to iterate over
-
-    ReactDOM.render(
-        elem(
-            'ul',
-            {},
-            retrieved_data.map(
-                (item, i) => elem(
-                    DoculectListItem, { doculect: item, key: i }, null
-                    )
-                )
-            ),
-        document.getElementById("doculect-finder-list")
-        );
-}
+  ReactDOM.render(
+    elem(
+      "ul",
+      {},
+      retrieved_data.map((item, i) =>
+        elem(DoculectListItem, { doculect: item, key: i }, null)
+      )
+    ),
+    document.getElementById("doculect-finder-list")
+  );
+};
 
 function DoculectListForm(props) {
-    const [query, setQuery] = React.useState("");
+  const [query, setQuery] = React.useState("");
 
-    const locale = getLocale();
+  const locale = getLocale();
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      if (query === "") {
-        renderListOfMatchingDoculects([]);
-      } else {
-        let url = `/${locale}/json_api/doculect_by_name/` + query
-        renderListOfMatchingDoculects(retrieve_data(url));
-      }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (query === "") {
+      renderListOfMatchingDoculects([]);
+    } else {
+      let url = `/${locale}/json_api/doculect_by_name/` + query;
+      fetchAndRenderResults(url);
     }
+  };
 
-    return elem(
-        "form",
-        { onSubmit: e => {handleSubmit(e)} },
-        elem("label", {}, doculectListFormLabels["formLabel"][locale]),
-        elem("br"),
-        elem("input", {name: "searchText", type: "text", onChange: e => setQuery(e.target.value)}),
-        elem("input", {type: "submit", value: doculectListFormLabels["buttonLabel"][locale]})
-    )
+  return elem(
+    "form",
+    {
+      onSubmit: (e) => {
+        handleSubmit(e);
+      },
+    },
+    elem("label", {}, doculectListFormLabels["formLabel"][locale]),
+    elem("br"),
+    elem("input", {
+      name: "searchText",
+      type: "text",
+      onChange: (e) => setQuery(e.target.value),
+    }),
+    elem("input", {
+      type: "submit",
+      value: doculectListFormLabels["buttonLabel"][locale],
+    })
+  );
 }
 
-ReactDOM.render(elem(DoculectListForm), document.getElementById("doculect-finder-form"));
+ReactDOM.render(
+  elem(DoculectListForm),
+  document.getElementById("doculect-finder-form")
+);
