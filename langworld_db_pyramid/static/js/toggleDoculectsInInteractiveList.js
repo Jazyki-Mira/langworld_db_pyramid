b5acf5a272a76a0987dc4a4514a7dd7fdb902081
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2022-08-14 14:15:30 +0300

    refactor: move list/legend switch to parent `page_with_map.jinja2`
    move <div> with the switch and <script> import into
    the template page_with_map.jinja2 that is parent
    for feature.jinja2 and families.jinja2.
    
    note that the JS script has now to ensure it only
    runs after full rendering of page. Otherwise
    it will not wait for the child template
    (feature.jinja2 or families.jinja2) to render.

--- langworld_db_pyramid/static/js/toggleDoculectsInInteractiveList.js
+++ langworld_db_pyramid/static/js/toggleDoculectsInInteractiveList.js
@@ -1,18 +1,25 @@
-const toggleSwitch = document.getElementById("doculects-in-list-toggle");
+/* This script is imported in a parent Jinja template,
+so including it without .onload will cause it to run
+while the child template is still not fully rendered.
+*/
 
-toggleSwitch.onclick = () => {
-  for (let ul of document.querySelectorAll("ul.doculects-in-group")) {
-    ul.classList.toggle("w3-hide");
-  }
+window.onload = () => {
+  const toggleSwitch = document.getElementById("doculects-in-list-toggle");
 
-  for (let heading of document.querySelectorAll("p.legend-heading")) {
-    heading.classList.toggle("w3-large");
-    heading.classList.toggle("w3-medium");
-  }
+  toggleSwitch.onclick = () => {
+    for (let ul of document.querySelectorAll("ul.doculects-in-group")) {
+      ul.classList.toggle("w3-hide");
+    }
+
+    for (let heading of document.querySelectorAll("p.legend-heading")) {
+      heading.classList.toggle("w3-large");
+      heading.classList.toggle("w3-medium");
+    }
+  };
+
+  const toggleSwitchContainer = document.getElementById(
+    "doculects-in-list-toggle-container"
+  );
+  const listContainer = document.getElementById("interactive-list");
+  listContainer.prepend(toggleSwitchContainer);
 };
-
-const toggleSwitchContainer = document.getElementById(
-  "doculects-in-list-toggle-container"
-);
-const listContainer = document.getElementById("interactive-list");
-listContainer.prepend(toggleSwitchContainer);
