52a7a24494c134e2a82af3e1fafbfe1dc3ec3b13
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2022-09-07 19:04:32 +0300

    dev: install `sqlalchemy_schemadisplay`, generate ERD for database

---
+++ langworld_db_pyramid/dbutils/generate_erd.py
@@ -0,0 +1,16 @@
+from sqlalchemy import MetaData
+from sqlalchemy_schemadisplay import create_schema_graph
+
+
+def generate_erd(path_to_database: str = 'sqlite:///../../langworld_db_pyramid.sqlite') -> None:
+    graph = create_schema_graph(
+        metadata=MetaData(path_to_database),
+        show_indexes=False,
+        rankdir='LR',
+        concentrate='False'
+    )
+    graph.write_png('erd.png')
+
+
+if __name__ == '__main__':
+    generate_erd()
