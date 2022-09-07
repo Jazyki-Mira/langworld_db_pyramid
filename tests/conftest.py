1d5613be806783ccbad192b4030b66797a5a87ec
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2022-09-07 16:34:02 +0300

    dev: mv PasteDeploy INI files to `wsgi_config` (NB reconfigure server!)
    move PasteDeploy .ini files to subdir
    note that this will require reconfiguring WSGI
    config file on the server

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ import os
 import alembic
 import alembic.config
 import alembic.command
+from pathlib import Path
 from pyramid.paster import get_appsettings
 from pyramid.scripting import prepare
 from pyramid.testing import DummyRequest, testConfig
@@ -24,7 +25,7 @@ def pytest_addoption(parser):
 @pytest.fixture(scope='session')
 def ini_file(request):
     # potentially grab this path from a pytest option
-    return os.path.abspath(request.config.option.ini or 'testing.ini')
+    return os.path.abspath(request.config.option.ini or str(Path('wsgi_config') / 'testing.ini'))
 
 
 @pytest.fixture(scope='session')
