5235c9cb74f5461b107a41c2b07e167af72cb198
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2022-09-10 19:58:25 +0300

    fix(not found view) fix locale, add dynamic home URL to template
    notfound_view is not included in routes.py, and hence
    the locale present in URL is not matched.
    This means that not found view is always in Russian.
    This can be fixed by directly looking up locale
    in relative URL of current page.

--- langworld_db_pyramid/views/notfound.py
+++ langworld_db_pyramid/views/notfound.py
@@ -4,4 +4,13 @@ from pyramid.view import notfound_view_config
 @notfound_view_config(renderer='langworld_db_pyramid:templates/404.jinja2')
 def notfound_view(request):
     request.response.status = 404
+
+    # This view is not registered in routes.py. This means that the common locale negotiator
+    # will not determine the English locale correctly because there is no `matchdict`
+    # from which English locale can be extracted.
+    # There may be a better solution, but it seems easy to just check the relative URL.
+    # It will only stop working if I change the URL system completely.
+    if request.path.startswith('/en/'):
+        request.locale_name = 'en'
+
     return {}
