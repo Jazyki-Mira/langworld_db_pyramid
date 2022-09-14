a90f2526136d552c339d220128236dc9a47477be
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2022-09-14 13:49:28 +0300

    minor(`mapbox_token.py`) add comment

--- langworld_db_pyramid/views/mapbox_token.py
+++ langworld_db_pyramid/views/mapbox_token.py
@@ -3,4 +3,5 @@ from pyramid.view import view_config
 
 @view_config(route_name='mapbox_token', renderer='json')
 def get_mapbox_token(request) -> str:
+    # value will be taken from `mapbox_access_token` key of `[app:main]` section in `.ini` file
     return request.registry.settings['mapbox_access_token']
