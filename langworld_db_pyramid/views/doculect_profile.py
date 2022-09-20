8e10898ccecb9bd2f5a29f6bd994810c628ad86f
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2022-09-20 18:09:15 +0300

    refactor(doculect profile) create dictionary to find comments quicker

--- langworld_db_pyramid/views/doculect_profile.py
+++ langworld_db_pyramid/views/doculect_profile.py
@@ -8,7 +8,21 @@ from langworld_db_pyramid.dbutils.query_helpers import get_all, get_by_man_id
 @view_config(route_name='doculect_profile', renderer='langworld_db_pyramid:templates/doculect.jinja2')
 @view_config(route_name='doculect_profile_localized', renderer='langworld_db_pyramid:templates/doculect.jinja2')
 def view_doculect_profile(request):
+    doculect = get_by_man_id(request=request, model=models.Doculect, man_id=request.matchdict['doculect_man_id'])
+
+    # Create a dictionary to easily find a comment for a given feature value and a given doculect:
+    # 1. Query to find comments only for this doculect
+    comments: list[models.DoculectFeatureValueComment] = get_all(
+        request,
+        select(models.DoculectFeatureValueComment).where(models.DoculectFeatureValueComment.doculect_id == doculect.id))
+    # 2. Map feature values to comments for this doculect.
+    comment_for_feature_value: dict[str, models.DoculectFeatureValueComment] = {
+        comment.feature_value: comment for comment in comments
+    }
+
     return {
-        'doculect': get_by_man_id(request=request, model=models.Doculect, man_id=request.matchdict['doculect_man_id']),
-        'categories': get_all(request, select(models.FeatureCategory).order_by(models.FeatureCategory.man_id))
+        'doculect': doculect,
+        'categories': get_all(request,
+                              select(models.FeatureCategory).order_by(models.FeatureCategory.man_id)),
+        'comment_for_feature_value': comment_for_feature_value
     }
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,9 +1,12 @@
 import pyramid.httpexceptions
 import pytest
+from sqlalchemy import select
 
+from langworld_db_pyramid.dbutils.query_helpers import get_by_man_id, _get_one
 from langworld_db_pyramid.models.doculect import Doculect
 from langworld_db_pyramid.models.family import Family
 from langworld_db_pyramid.models.feature_category import FeatureCategory
+from langworld_db_pyramid.models.feature_value import FeatureValue
 from langworld_db_pyramid.views.doculects_list import get_doculects_by_substring, view_all_doculects_list
 from langworld_db_pyramid.views.doculects_map import get_doculects_for_map
 from langworld_db_pyramid.views.doculect_profile import view_doculect_profile
@@ -212,12 +215,19 @@ def test_view_all_doculects_list(dummy_request, setup_models_once_for_test_modul
 def test_view_doculect_profile(dummy_request, setup_models_once_for_test_module):
     dummy_request.matchdict['doculect_man_id'] = 'aragonese'
 
-    info = view_doculect_profile(dummy_request)
-    doculect = info['doculect']
+    data = view_doculect_profile(dummy_request)
+    doculect = data['doculect']
     assert isinstance(doculect, Doculect)
     assert doculect.name_en == 'Aragonese'
 
-    assert len(info['categories']) == 14
+    assert len(data['categories']) == 14
+
+    comment_for_value = data['comment_for_feature_value']
+    assert len(comment_for_value) == 21
+
+    for man_id in ('K-1-3', 'K-2-3'):
+        value = get_by_man_id(request=dummy_request, model=FeatureValue, man_id=man_id)
+        assert value in comment_for_value
 
 
 def test_view_doculect_profile_raises_not_found(dummy_request, setup_models_once_for_test_module):
