d2c8a7711834476e8ca969001380a42b9c176995
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2024-08-08 11:23:18 +0300

    add timeouts, change type of event for listener

--- langworld_db_pyramid/static/js/doculectMapAll.js
+++ langworld_db_pyramid/static/js/doculectMapAll.js
@@ -3,20 +3,24 @@ import renderMapWithList from "./tools/renderMapWithList.js";
 
 renderMapWithList("all");
 
-// FIXME this only works after Ctrl+F5
-// using event listener, otherwise it will run while the template is still not fully rendered
-window.addEventListener("load", () => {
+/* using event listener to wait for DOM the template to be rendered
+PLUS a timeout to wait for data to be loaded.
+Returning a promise from `renderMapWithList` and using `.then()` on it doesn't work: 
+throws `<name of promise> is undefined`.
+TODO: maybe some changes could be made to `renderMapWithList()`
+*/
+window.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    // default behavior of list is to have all items collapsed. Here it's better to have them expanded
+    const markerGroups = document.querySelectorAll(
+      "ul.doculects-in-group.w3-ul.w3-hide"
+    );
 
-  // default behavior of list is to have all items collapsed. Here it's better to have them expanded
-  const markerGroups = document.querySelectorAll(
-    "ul.doculects-in-group.w3-ul.w3-hide"
-  );
+    markerGroups.forEach((ul) => {
+      ul.classList.remove("w3-hide");
+    });
 
-  markerGroups.forEach((ul) => {
-    console.log(ul.classList);
-    ul.classList.remove("w3-hide");
-  });
-
-  // There is only one group of doculects, so "expand/collapse" buttons are not needed.
-  hideExpandCollapseContainer();
+    // There is only one group of doculects, so "expand/collapse" buttons are not needed.
+    hideExpandCollapseContainer();
+  }, 750);
 });
--- langworld_db_pyramid/static/js/toggleDoculectsInInteractiveList.js
+++ langworld_db_pyramid/static/js/toggleDoculectsInInteractiveList.js
@@ -1,37 +1,39 @@
 /* This script is imported in a parent Jinja template,
-so including it without eventListener for "load" will cause it to run
+so including it without eventListener will cause it to run
 while the child template is still not fully rendered.
-
-I use .addEventListener instead of .onload to allow potential multiple
-actions after window is loaded.
 */
 
-window.addEventListener("load", () => {
-  const expandAllButton = document.getElementById(
-    "doculect-list-expand-all-button"
-  );
-  const collapseAllButton = document.getElementById(
-    "doculect-list-collapse-all-button"
-  );
-  const markerGroups = document.querySelectorAll(
-    "ul.doculects-in-group.w3-ul.w3-hide"
-  );
+window.addEventListener("DOMContentLoaded", () => {
+  /* Additional timeout is needed for the <ul>'s to be rendered.
+  TODO think about reworking without explicit timeout
+  */
+  setTimeout(() => {
+    const expandAllButton = document.getElementById(
+      "doculect-list-expand-all-button"
+    );
+    const collapseAllButton = document.getElementById(
+      "doculect-list-collapse-all-button"
+    );
+    const markerGroups = document.querySelectorAll(
+      "ul.doculects-in-group.w3-ul.w3-hide"
+    );
 
-  expandAllButton.onclick = () => {
-    markerGroups.forEach((ul) => {
-      ul.classList.remove("w3-hide");
-    });
-  };
+    expandAllButton.onclick = () => {
+      markerGroups.forEach((ul) => {
+        ul.classList.remove("w3-hide");
+      });
+    };
 
-  collapseAllButton.onclick = () => {
-    markerGroups.forEach((ul) => {
-      ul.classList.add("w3-hide");
-    });
-  };
+    collapseAllButton.onclick = () => {
+      markerGroups.forEach((ul) => {
+        ul.classList.add("w3-hide");
+      });
+    };
 
-  const expandCollapseContainer = document.getElementById(
-    "doculect-list-expand-collapse-container"
-  );
-  const listContainer = document.getElementById("interactive-list");
-  if (listContainer != null) listContainer.prepend(expandCollapseContainer);
+    const expandCollapseContainer = document.getElementById(
+      "doculect-list-expand-collapse-container"
+    );
+    const listContainer = document.getElementById("interactive-list");
+    if (listContainer != null) listContainer.prepend(expandCollapseContainer);
+  }, 750);
 });
