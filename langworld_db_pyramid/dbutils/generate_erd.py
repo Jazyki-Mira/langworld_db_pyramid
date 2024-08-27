360d1b650d1237ce3bcb31337c48b35bee2bbdb7
Dmitry K <58207913+lemontree210@users.noreply.github.com>
2024-08-27 16:18:43 +0300

    fix generation of ERD (#252)
    closes #157

--- langworld_db_pyramid/dbutils/generate_erd.py
+++ langworld_db_pyramid/dbutils/generate_erd.py
@@ -1,12 +1,27 @@
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, create_engine
 from sqlalchemy_schemadisplay import create_schema_graph
 
 
 def generate_erd(path_to_database: str = "sqlite:///../../langworld_db_pyramid.sqlite") -> None:
+    metadata = _prepare_metadata(path_to_database)
+
     graph = create_schema_graph(
-        metadata=MetaData(path_to_database), show_indexes=False, rankdir="LR", concentrate="False"
+        metadata=metadata, show_indexes=False, rankdir="LR", concentrate="False"
     )
     graph.write_png("erd.png")
+
+
+def _prepare_metadata(path_to_database: str) -> MetaData:  # pragma: no cover
+    """Prepare metadata for further ERD generation.
+
+    1. initialize an engine, 2. reflect DB's metadata, 3. bind all tables' metadata to the engine
+    """
+    engine = create_engine(path_to_database)
+    metadata = MetaData()
+    metadata.reflect(bind=engine)
+    for table in metadata.tables.values():
+        table.metadata.bind = engine
+    return metadata
 
 
 if __name__ == "__main__":
