8397d302484f7764ae03e36e80a631a07c9fc3cc
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2025-05-27 16:00:42 +0300

    add pipenv run to bash scripts

--- bash_scripts/init_compile.sh
+++ bash_scripts/init_compile.sh
@@ -4,10 +4,10 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd ${SCRIPT_DIR}/../
 
 echo "Applying alembic migrations (if any)"
-alembic -c config/production.ini upgrade head
+pipenv run alembic -c config/production.ini upgrade head
 
 echo "Re-populating the database"
-initialize_langworld_db_pyramid_db config/production.ini
+pipenv run initialize_langworld_db_pyramid_db config/production.ini
 
 echo "Compiling translated strings for internationalization"
-pybabel compile --directory=langworld_db_pyramid/locale --locale=en
+pipenv run pybabel compile --directory=langworld_db_pyramid/locale --locale=en
--- bash_scripts/pull_data.sh
+++ bash_scripts/pull_data.sh
@@ -9,6 +9,6 @@ echo "Downloading data: an editor will be opened for you to enter a commit messa
 git subtree pull --prefix langworld_db_data https://github.com/jazyki-mira/langworld_db_data/ master --squash
 
 echo "Re-populating the database"
-initialize_langworld_db_pyramid_db config/production.ini
+pipenv run initialize_langworld_db_pyramid_db config/production.ini
 
 echo "Done. Make sure you push the updated tree to remote repository."
